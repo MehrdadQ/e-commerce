02e610e537cd4767de2ae4c70149f11408615476
Mehrdad Ghannad <mehrdad37ghannad@gmail.com>
2021-10-04 22:42:05 -0400

    Updates

---
+++ static/js/main.ff38b2f9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpecommerce=this.webpackJsonpecommerce||[]).push([[0],{9:function(e,c,t){"use strict";t.r(c);var s=t(0),n=t.n(s),r=t(3),o=t.n(r),i=t(1),d=function(){return Object(i.jsx)("div",{children:"E-comsssss"})};o.a.render(Object(i.jsx)(n.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.ff38b2f9.chunk.js.map
